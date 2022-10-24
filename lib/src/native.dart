6c75fdd9c1c04db79acd58e55c2c889162fbb9be
jenkins <dantes.lu@163.com>
2022-10-24 17:05:12 +0800

    reverse to ffi 1.0.0 and ffigen ^2.4.2

--- lib/src/native.dart
+++ lib/src/native.dart
@@ -19,21 +19,17 @@ class NativeFlKvDB {
       : _lookup = lookup;
 
   ffi.Pointer<FlKv> db_open(
-    ffi.Pointer<ffi.Char> name,
+    ffi.Pointer<ffi.Int8> name,
     bool memory,
   ) {
     return _db_open(
       name,
-      memory,
+      memory ? 1 : 0,
     );
   }
 
-  late final _db_openPtr = _lookup<
-      ffi.NativeFunction<
-          ffi.Pointer<FlKv> Function(
-              ffi.Pointer<ffi.Char>, ffi.Bool)>>('db_open');
-  late final _db_open = _db_openPtr
-      .asFunction<ffi.Pointer<FlKv> Function(ffi.Pointer<ffi.Char>, bool)>();
+  late final _db_open_ptr = _lookup<ffi.NativeFunction<_c_db_open>>('db_open');
+  late final _dart_db_open _db_open = _db_open_ptr.asFunction<_dart_db_open>();
 
   bool db_put(
     ffi.Pointer<FlKv> flkv,
@@ -41,29 +37,24 @@ class NativeFlKvDB {
     ffi.Pointer<KvBuffer> value,
   ) {
     return _db_put(
-      flkv,
-      key,
-      value,
-    );
+          flkv,
+          key,
+          value,
+        ) !=
+        0;
   }
 
-  late final _db_putPtr = _lookup<
-      ffi.NativeFunction<
-          ffi.Bool Function(ffi.Pointer<FlKv>, ffi.Pointer<KvBuffer>,
-              ffi.Pointer<KvBuffer>)>>('db_put');
-  late final _db_put = _db_putPtr.asFunction<
-      bool Function(
-          ffi.Pointer<FlKv>, ffi.Pointer<KvBuffer>, ffi.Pointer<KvBuffer>)>();
+  late final _db_put_ptr = _lookup<ffi.NativeFunction<_c_db_put>>('db_put');
+  late final _dart_db_put _db_put = _db_put_ptr.asFunction<_dart_db_put>();
 
   ffi.Pointer<FlKvBatch> db_create_batch() {
     return _db_create_batch();
   }
 
-  late final _db_create_batchPtr =
-      _lookup<ffi.NativeFunction<ffi.Pointer<FlKvBatch> Function()>>(
-          'db_create_batch');
-  late final _db_create_batch =
-      _db_create_batchPtr.asFunction<ffi.Pointer<FlKvBatch> Function()>();
+  late final _db_create_batch_ptr =
+      _lookup<ffi.NativeFunction<_c_db_create_batch>>('db_create_batch');
+  late final _dart_db_create_batch _db_create_batch =
+      _db_create_batch_ptr.asFunction<_dart_db_create_batch>();
 
   bool batch_add_kv(
     ffi.Pointer<FlKvBatch> batch,
@@ -71,52 +62,49 @@ class NativeFlKvDB {
     ffi.Pointer<KvBuffer> value,
   ) {
     return _batch_add_kv(
-      batch,
-      key,
-      value,
-    );
+          batch,
+          key,
+          value,
+        ) !=
+        0;
   }
 
-  late final _batch_add_kvPtr = _lookup<
-      ffi.NativeFunction<
-          ffi.Bool Function(ffi.Pointer<FlKvBatch>, ffi.Pointer<KvBuffer>,
-              ffi.Pointer<KvBuffer>)>>('batch_add_kv');
-  late final _batch_add_kv = _batch_add_kvPtr.asFunction<
-      bool Function(ffi.Pointer<FlKvBatch>, ffi.Pointer<KvBuffer>,
-          ffi.Pointer<KvBuffer>)>();
+  late final _batch_add_kv_ptr =
+      _lookup<ffi.NativeFunction<_c_batch_add_kv>>('batch_add_kv');
+  late final _dart_batch_add_kv _batch_add_kv =
+      _batch_add_kv_ptr.asFunction<_dart_batch_add_kv>();
 
   bool batch_clear(
     ffi.Pointer<FlKvBatch> batch,
   ) {
     return _batch_clear(
-      batch,
-    );
+          batch,
+        ) !=
+        0;
   }
 
-  late final _batch_clearPtr =
-      _lookup<ffi.NativeFunction<ffi.Bool Function(ffi.Pointer<FlKvBatch>)>>(
-          'batch_clear');
-  late final _batch_clear =
-      _batch_clearPtr.asFunction<bool Function(ffi.Pointer<FlKvBatch>)>();
+  late final _batch_clear_ptr =
+      _lookup<ffi.NativeFunction<_c_batch_clear>>('batch_clear');
+  late final _dart_batch_clear _batch_clear =
+      _batch_clear_ptr.asFunction<_dart_batch_clear>();
 
   bool db_put_batch(
     ffi.Pointer<FlKv> flkv,
     ffi.Pointer<FlKvBatch> batch,
-    bool sync1,
+    bool sync_1,
   ) {
     return _db_put_batch(
-      flkv,
-      batch,
-      sync1,
-    );
+          flkv,
+          batch,
+          sync_1 ? 1 : 0,
+        ) !=
+        0;
   }
 
-  late final _db_put_batchPtr = _lookup<
-      ffi.NativeFunction<
-          ffi.Bool Function(ffi.Pointer<FlKv>, ffi.Pointer<FlKvBatch>,
-              ffi.Bool)>>('db_put_batch');
-  late final _db_put_batch = _db_put_batchPtr.asFunction<
-      bool Function(ffi.Pointer<FlKv>, ffi.Pointer<FlKvBatch>, bool)>();
+  late final _db_put_batch_ptr =
+      _lookup<ffi.NativeFunction<_c_db_put_batch>>('db_put_batch');
+  late final _dart_db_put_batch _db_put_batch =
+      _db_put_batch_ptr.asFunction<_dart_db_put_batch>();
 
   ffi.Pointer<KvBuffer> db_get(
     ffi.Pointer<FlKv> flkv,
@@ -128,35 +116,29 @@ class NativeFlKvDB {
     );
   }
 
-  late final _db_getPtr = _lookup<
-      ffi.NativeFunction<
-          ffi.Pointer<KvBuffer> Function(
-              ffi.Pointer<FlKv>, ffi.Pointer<KvBuffer>)>>('db_get');
-  late final _db_get = _db_getPtr.asFunction<
-      ffi.Pointer<KvBuffer> Function(
-          ffi.Pointer<FlKv>, ffi.Pointer<KvBuffer>)>();
+  late final _db_get_ptr = _lookup<ffi.NativeFunction<_c_db_get>>('db_get');
+  late final _dart_db_get _db_get = _db_get_ptr.asFunction<_dart_db_get>();
 
   bool db_delete(
     ffi.Pointer<FlKv> flkv,
     ffi.Pointer<KvBuffer> key,
   ) {
     return _db_delete(
-      flkv,
-      key,
-    );
+          flkv,
+          key,
+        ) !=
+        0;
   }
 
-  late final _db_deletePtr = _lookup<
-      ffi.NativeFunction<
-          ffi.Bool Function(
-              ffi.Pointer<FlKv>, ffi.Pointer<KvBuffer>)>>('db_delete');
-  late final _db_delete = _db_deletePtr
-      .asFunction<bool Function(ffi.Pointer<FlKv>, ffi.Pointer<KvBuffer>)>();
+  late final _db_delete_ptr =
+      _lookup<ffi.NativeFunction<_c_db_delete>>('db_delete');
+  late final _dart_db_delete _db_delete =
+      _db_delete_ptr.asFunction<_dart_db_delete>();
 
   int db_list(
     ffi.Pointer<FlKv> flkv,
     ffi.Pointer<ffi.Pointer<Row>> buffer,
-    ffi.Pointer<uintptr_t> size,
+    ffi.Pointer<ffi.Uint64> size,
   ) {
     return _db_list(
       flkv,
@@ -165,13 +147,8 @@ class NativeFlKvDB {
     );
   }
 
-  late final _db_listPtr = _lookup<
-      ffi.NativeFunction<
-          ffi.Int32 Function(ffi.Pointer<FlKv>, ffi.Pointer<ffi.Pointer<Row>>,
-              ffi.Pointer<uintptr_t>)>>('db_list');
-  late final _db_list = _db_listPtr.asFunction<
-      int Function(ffi.Pointer<FlKv>, ffi.Pointer<ffi.Pointer<Row>>,
-          ffi.Pointer<uintptr_t>)>();
+  late final _db_list_ptr = _lookup<ffi.NativeFunction<_c_db_list>>('db_list');
+  late final _dart_db_list _db_list = _db_list_ptr.asFunction<_dart_db_list>();
 
   void release_list(
     ffi.Pointer<Row> buffer,
@@ -183,39 +160,38 @@ class NativeFlKvDB {
     );
   }
 
-  late final _release_listPtr = _lookup<
-          ffi.NativeFunction<ffi.Void Function(ffi.Pointer<Row>, uintptr_t)>>(
-      'release_list');
-  late final _release_list =
-      _release_listPtr.asFunction<void Function(ffi.Pointer<Row>, int)>();
+  late final _release_list_ptr =
+      _lookup<ffi.NativeFunction<_c_release_list>>('release_list');
+  late final _dart_release_list _release_list =
+      _release_list_ptr.asFunction<_dart_release_list>();
 
   bool db_flush(
     ffi.Pointer<FlKv> flkv,
   ) {
     return _db_flush(
-      flkv,
-    );
+          flkv,
+        ) !=
+        0;
   }
 
-  late final _db_flushPtr =
-      _lookup<ffi.NativeFunction<ffi.Bool Function(ffi.Pointer<FlKv>)>>(
-          'db_flush');
-  late final _db_flush =
-      _db_flushPtr.asFunction<bool Function(ffi.Pointer<FlKv>)>();
+  late final _db_flush_ptr =
+      _lookup<ffi.NativeFunction<_c_db_flush>>('db_flush');
+  late final _dart_db_flush _db_flush =
+      _db_flush_ptr.asFunction<_dart_db_flush>();
 
   bool db_close(
     ffi.Pointer<FlKv> flkv,
   ) {
     return _db_close(
-      flkv,
-    );
+          flkv,
+        ) !=
+        0;
   }
 
-  late final _db_closePtr =
-      _lookup<ffi.NativeFunction<ffi.Bool Function(ffi.Pointer<FlKv>)>>(
-          'db_close');
-  late final _db_close =
-      _db_closePtr.asFunction<bool Function(ffi.Pointer<FlKv>)>();
+  late final _db_close_ptr =
+      _lookup<ffi.NativeFunction<_c_db_close>>('db_close');
+  late final _dart_db_close _db_close =
+      _db_close_ptr.asFunction<_dart_db_close>();
 }
 
 class FlKv extends ffi.Opaque {}
@@ -224,16 +200,130 @@ class FlKvBatch extends ffi.Opaque {}
 
 /// Array struct
 class KvBuffer extends ffi.Struct {
-  external ffi.Pointer<ffi.UnsignedChar> data;
+  external ffi.Pointer<ffi.Uint8> data;
 
-  @uintptr_t()
+  @ffi.Uint64()
   external int length;
 }
-
-typedef uintptr_t = ffi.UnsignedLong;
 
 class Row extends ffi.Struct {
   external KvBuffer key;
 
   external KvBuffer value;
 }
+
+typedef _c_db_open = ffi.Pointer<FlKv> Function(
+  ffi.Pointer<ffi.Int8> name,
+  ffi.Uint8 memory,
+);
+
+typedef _dart_db_open = ffi.Pointer<FlKv> Function(
+  ffi.Pointer<ffi.Int8> name,
+  int memory,
+);
+
+typedef _c_db_put = ffi.Uint8 Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<KvBuffer> key,
+  ffi.Pointer<KvBuffer> value,
+);
+
+typedef _dart_db_put = int Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<KvBuffer> key,
+  ffi.Pointer<KvBuffer> value,
+);
+
+typedef _c_db_create_batch = ffi.Pointer<FlKvBatch> Function();
+
+typedef _dart_db_create_batch = ffi.Pointer<FlKvBatch> Function();
+
+typedef _c_batch_add_kv = ffi.Uint8 Function(
+  ffi.Pointer<FlKvBatch> batch,
+  ffi.Pointer<KvBuffer> key,
+  ffi.Pointer<KvBuffer> value,
+);
+
+typedef _dart_batch_add_kv = int Function(
+  ffi.Pointer<FlKvBatch> batch,
+  ffi.Pointer<KvBuffer> key,
+  ffi.Pointer<KvBuffer> value,
+);
+
+typedef _c_batch_clear = ffi.Uint8 Function(
+  ffi.Pointer<FlKvBatch> batch,
+);
+
+typedef _dart_batch_clear = int Function(
+  ffi.Pointer<FlKvBatch> batch,
+);
+
+typedef _c_db_put_batch = ffi.Uint8 Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<FlKvBatch> batch,
+  ffi.Uint8 sync_1,
+);
+
+typedef _dart_db_put_batch = int Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<FlKvBatch> batch,
+  int sync_1,
+);
+
+typedef _c_db_get = ffi.Pointer<KvBuffer> Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<KvBuffer> key,
+);
+
+typedef _dart_db_get = ffi.Pointer<KvBuffer> Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<KvBuffer> key,
+);
+
+typedef _c_db_delete = ffi.Uint8 Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<KvBuffer> key,
+);
+
+typedef _dart_db_delete = int Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<KvBuffer> key,
+);
+
+typedef _c_db_list = ffi.Int32 Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<ffi.Pointer<Row>> buffer,
+  ffi.Pointer<ffi.Uint64> size,
+);
+
+typedef _dart_db_list = int Function(
+  ffi.Pointer<FlKv> flkv,
+  ffi.Pointer<ffi.Pointer<Row>> buffer,
+  ffi.Pointer<ffi.Uint64> size,
+);
+
+typedef _c_release_list = ffi.Void Function(
+  ffi.Pointer<Row> buffer,
+  ffi.Uint64 size,
+);
+
+typedef _dart_release_list = void Function(
+  ffi.Pointer<Row> buffer,
+  int size,
+);
+
+typedef _c_db_flush = ffi.Uint8 Function(
+  ffi.Pointer<FlKv> flkv,
+);
+
+typedef _dart_db_flush = int Function(
+  ffi.Pointer<FlKv> flkv,
+);
+
+typedef _c_db_close = ffi.Uint8 Function(
+  ffi.Pointer<FlKv> flkv,
+);
+
+typedef _dart_db_close = int Function(
+  ffi.Pointer<FlKv> flkv,
+);
